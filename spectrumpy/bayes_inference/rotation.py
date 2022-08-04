8c21a4ad6ff7c5865354c629f0f669371ccc3566
d-sanfratello <d.sanfratello@studenti.unipi.it>
2022-08-04 20:12:42 +0200

    Created rotation posterior to determine rotation angle to re-orient spectrum

---
+++ spectrumpy/bayes_inference/rotation.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+from cpnest import model
+
+
+class RotationPosterior(model.Model):
+    def __init__(self, x, y, sx, sy, bounds):
+        self.x = x
+        self.y = y
+
+        if hasattr(sx, "__iter__"):
+            self.sx = sx
+        else:
+            self.sx = np.ones(x.shape[0]) * sx
+
+        if hasattr(sy, "__iter__"):
+            self.sy = sy
+        else:
+            self.sy = np.ones(y.shape[0]) * sy
+
+        self.bounds = bounds
+        self.names = ['m', 'q']
+
+    def log_likelihood(self, param):
+        m = param['m']
+        q = param['q']
+        s2 = m**2 * self.sx**2 + self.sy**2
+
+        exp = m * self.x + q
+        log_l = -0.5 * (self.y - exp)**2 / s2
+        log_l -= 2 * np.pi
+        log_l -= np.sqrt(s2)
+
+        return log_l.sum()
+
+    def log_prior(self, param):
+        logP = super(RotationPosterior, self).log_prior(param)
+        if np.isfinite(logP):
+            logP = 0
+            # logP -= np.log(param['E(B-V)'])
+
+        return logP
+
+    @staticmethod
+    def __linear(x, param):
+        return x * param['m'] + param['q']
