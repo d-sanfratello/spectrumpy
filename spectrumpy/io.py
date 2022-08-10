29844d5aa05d6d9e5d55fadfe3c03070bacfdc70
d-sanfratello <d.sanfratello@studenti.unipi.it>
2022-08-10 21:10:49 +0200

    changed name and improved class to open file

---
+++ spectrumpy/io.py
@@ -0,0 +1,20 @@
+from astropy.io import fits
+
+from spectrumpy.core.spectrum_image import SpectrumImage
+
+
+class SpectrumPath:
+    def __init__(self,
+                 spectrum_path,
+                 calibration=None,
+                 is_lamp=False):
+        with fits.open(spectrum_path) as s_file:
+            self.hdu_list = [hdu for hdu in s_file]
+            self.headers = [hdu.header for hdu in self.hdu_list]
+            self.data = [hdu.data for hdu in self.hdu_list]
+
+        self.images = {
+            f'{i}': SpectrumImage(img, is_lamp, calibration)
+            for i, img in enumerate(self.data)
+            if self.headers[i]['NAXIS'] == 2
+        }
