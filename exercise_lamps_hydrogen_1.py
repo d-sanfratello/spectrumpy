cf6c7cc473c429352d84a005073368882e03b9c0
d-sanfratello <d.sanfratello@studenti.unipi.it>
2022-08-16 22:56:07 +0200

    Added small angle correction from vertical direction after main rotation. Updated exercise with better slicing after higher resolution rotation.

--- exercise_lamps_hydrogen_1.py
+++ exercise_lamps_hydrogen_1.py
@@ -21,6 +21,7 @@ lamps = {n: l for n, l in zip(names, lamps_path)}
 
 samples_folder = cwd.joinpath('exercise_data')
 rot_samples = samples_folder.joinpath('rot_samples.h5')
+rot_corr_samples = samples_folder.joinpath('rot_corr_samples.h5')
 
 find_angle = False
 
@@ -72,43 +73,166 @@ if __name__ == "__main__":
     models = np.array(
         [Linear.func(x, p[0], p[1]) for p in l_pars]
     )
-    hydr.show(figsize=figsize_sbs, model=models, x=x,
-              save=True, name='./exercise_data/image_hydr.pdf')
+    hydr.show(figsize=figsize_sbs,
+              model=models,
+              x=x,
+              show=False,
+              save=True,
+              name='./exercise_data/image_hydr.pdf',
+              title="Hydrogen lamp image")
 
+    # Rotating image by alpha
     hydr_rotated = hydr.rotate_image(alpha)
     hydr_rotated.show(figsize=figsize_sbs,
                       show=False,
                       save=True,
-                      name='./exercise_data/rotated_hydr.pdf')
+                      name='./exercise_data/rotated_hydr.pdf',
+                      title="Rotated hydrogen lamp image")
 
-    crops_y = [1125, 1758]
+    # Checking if there are still residual effects taking three slices
+    # at the top, middle and bottom of the spectrum.
+    slices = [1150, 1250, 1350, 1450, 1550, 1650, 1768]
+    mid_line = 1450
+    hydr_corr_slices = {
+        line: hydr_rotated.crop_image(crop_y=[slices[n_line] - 1,
+                                      slices[n_line]])
+        for n_line, line in enumerate(slices)
+    }
+
+    int_models = {
+        line: hydr_slice.run_integration()
+        for line, hydr_slice in hydr_corr_slices.items()
+    }
+
+    for line in int_models.keys():
+        exec(f"def slice_{line}(x): return int_models[{line}].spectrum")
+
+    models = [
+        eval(f"slice_{line}") for line in int_models.keys()
+    ]
+
+    mid_model = int_models[mid_line]
+    x_len = len(mid_model.spectrum)
+    x_sup = x_len - 1
+    models.pop(slices.index(mid_line))
+    mid_model.show(figsize=figsize_sbs,
+                   model=models,
+                   x=np.linspace(0, x_sup, x_len),
+                   legend=True,
+                   show=False,
+                   save=True,
+                   name='./exercise_data/hydr_align_slices.pdf',
+                   title="Slices of rotated hydrogen spectrum image")
+
+    angle_correction = False
+    if angle_correction:
+        y = [1744, 1745, 1746, 1747, 1750, 1752, 1754]
+        sy = [2, 4, 4, 3, 3, 2, 2]
+        x = [1768, 1650, 1550, 1450, 1350, 1250, 1150]
+        sx = [0, 0, 0, 0, 0, 0, 0]
+
+        job, alpha_corr = hydr.find_rotation_angle(
+            x, y, sy, sx,
+            bounds=[[-2, 2], [1e3, 2e3]],
+            verbose=1,
+            show=True,
+            save=True,
+            nlive=1000,
+            name='./exercise_data/joint_rotation_correction.pdf'
+        )
+
+        post = job.posterior_samples.ravel()
+        l_pars = np.column_stack([post['m'], post['q']])
+
+        with h5py.File(rot_corr_samples, 'w') as hf:
+            hf.create_dataset('line params', data=l_pars)
+            hf.create_dataset('alpha_corr',
+                              data=np.asarray(alpha_corr))
+    else:
+        with h5py.File(rot_corr_samples, 'r') as hf:
+            l_pars = hf['line params'][:]
+            alpha_corr = hf['alpha_corr'][:]
+
+    print(f"alpha_corr = {alpha_corr[0]:.3e} "
+          f"(+){alpha_corr[2]:.3e} (-){alpha_corr[1]:.3e}")
+    alpha_corr = alpha_corr[0]
+
+    # Rotating image by alphacorr
+    hydr_rotated = hydr.rotate_image(alpha-alpha_corr)
+    hydr_rotated.show(figsize=figsize_sbs,
+                      show=False,
+                      save=True,
+                      name='./exercise_data/corr_rotated_hydr.pdf',
+                      title="Rotated hydrogen lamp image")
+
+    check_alignment = False
+    if check_alignment:
+        slices = [1130, 1230, 1330, 1430, 1530, 1630, 1730]
+        mid_line = 1430
+        hydr_corr_slices = {
+            line: hydr_rotated.crop_image(crop_y=[slices[n_line] - 1,
+                                                  slices[n_line]])
+            for n_line, line in enumerate(slices)
+        }
+
+        int_models = {
+            line: hydr_slice.run_integration()
+            for line, hydr_slice in hydr_corr_slices.items()
+        }
+
+        for line in int_models.keys():
+            exec(f"def slice_{line}(x): return int_models[{line}].spectrum")
+
+        models = [
+            eval(f"slice_{line}") for line in int_models.keys()
+        ]
+
+        mid_model = int_models[mid_line]
+        x_len = len(mid_model.spectrum)
+        x_sup = x_len - 1
+        models.pop(slices.index(mid_line))
+        mid_model.show(figsize=figsize_sbs,
+                       model=models,
+                       x=np.linspace(0, x_sup, x_len),
+                       legend=True,
+                       show=True,
+                       save=False,
+                       title="Slices of rotated hydrogen spectrum image"
+                             " with correction")
+
+    crops_y = [1330, 1530]
     hydr_cropped = hydr_rotated.crop_image(crop_y=crops_y)
     hydr_cropped.show(figsize=figsize_sbs,
                       show=False,
                       save=True,
-                      name='./exercise_data/cropped_hydr.pdf')
+                      name='./exercise_data/cropped_hydr.pdf',
+                      title="Cropped hydrogen lamp image")
 
-    ylims = hydr_cropped.image.shape[0]
-    hydr_cropped_up = hydr_cropped.crop_image(crop_y=[ylims-1, ylims])
-    hydr_cropped_dw = hydr_cropped.crop_image(crop_y=[21, 22])
+    show_top_and_bottom = False
+    if show_top_and_bottom:
+        ylims = hydr_cropped.image.shape[0]
+        hydr_cropped_up = hydr_cropped.crop_image(crop_y=[ylims-1, ylims])
+        hydr_cropped_dw = hydr_cropped.crop_image(crop_y=[21, 22])
 
-    sp_ref_up = hydr_cropped_up.run_integration()
-    sp_ref_dw = hydr_cropped_dw.run_integration()
+        sp_ref_up = hydr_cropped_up.run_integration()
+        sp_ref_dw = hydr_cropped_dw.run_integration()
 
-    def sp_slice_dw(x):
-        return sp_ref_dw.spectrum
+        def sp_slice_dw(x):
+            return sp_ref_dw.spectrum
 
-    sp_ref_up.show(figsize=figsize_sbs,
-                   model=sp_slice_dw,
-                   x=np.linspace(0,
-                                 len(sp_ref_dw.spectrum) - 1,
-                                 len(sp_ref_dw.spectrum)),
-                   show=True,
-                   save=True,
-                   name='./exercise_data/hydr_spectrum_slices.pdf')
+        sp_ref_up.show(figsize=figsize_sbs,
+                       model=sp_slice_dw,
+                       x=np.linspace(0,
+                                     len(sp_ref_dw.spectrum) - 1,
+                                     len(sp_ref_dw.spectrum)),
+                       show=True,
+                       save=True,
+                       name='./exercise_data/hydr_spectrum_slices.pdf',
+                       title="Slices of cropped hydrogen spectrum image")
 
     sp = hydr_cropped.run_integration()
     sp.show(figsize=figsize_sbs,
-            show=False,
+            show=True,
             save=True,
-            name='./exercise_data/int_spectrum_hydr.pdf')
+            name='./exercise_data/int_spectrum_hydr.pdf',
+            title="Hydrogen lamp spectrum")
