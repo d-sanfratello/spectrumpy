9ca5c4e679bf4b4ba4419ffee7a67e7b8996078c
d-sanfratello <d.sanfratello@studenti.unipi.it>
2022-09-27 13:34:51 +0200

    updated setup_requires to check for setuptools_scm. Updated numpy version and downgraded python version.

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
 import numpy as np
 from setuptools import setup
-# from setuptools.command.build_ext import build_ext as _build_ext
+from setuptools.command.build_ext import build_ext as _build_ext
 from codecs import open
 
 try:
     import cpnest
 except ImportError:
     raise Exception("This package needs `cpnest`. To install it follow "
-    "instructions at "
-    "https://github.com/johnveitch/cpnest/tree/massively_parallel.")
+                    "instructions at https://github.com/johnveitch/cpnest/tree/massively_parallel.")
 
-#
-# # see https://stackoverflow.com/a/21621689/1862861 for why this is here
-# class build_ext(_build_ext):
-#     def finalize_options(self):
-#         _build_ext.finalize_options(self)
-#         # Prevent numpy from thinking it is still in its setup process:
-#         __builtins__.__NUMPY_SETUP__ = False
-#         self.include_dirs.append(np.get_include())
+
+# see https://stackoverflow.com/a/21621689/1862861 for why this is here
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        self.include_dirs.append(np.get_include())
 
 
 with open("requirements.txt") as requires_file:
@@ -30,10 +29,10 @@ setup(
     author='Daniele Sanfratello',
     author_email='d.sanfratello@studenti.unipi.it',
     url='https://gitlab.com/da.sanfratello/spectrumpy',
-    python_requires='>=3.10.4',
+    python_requires='~=3.7',
     packages=['spectrumpy'],
     install_requires=requirements,
     include_dirs=[np.get_include()],
-    setup_requires=['numpy>=1.23.1'],
+    setup_requires=['numpy~=1.21.5', 'setuptools_scm'],
     entry_points={},
     )
