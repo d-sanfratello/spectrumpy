c43268a29a3607e4650b6638056799691511d462
d-sanfratello <d.sanfratello@studenti.unipi.it>
2022-10-04 23:24:26 +0200

    Updated setup with figaro import check. TODO: check for commented-out lines

--- setup.py
+++ setup.py
@@ -1,26 +1,44 @@
 import numpy as np
 from setuptools import setup
-from setuptools.command.build_ext import build_ext as _build_ext
+# from setuptools.command.build_ext import build_ext as _build_ext
 from codecs import open
 
 try:
     import cpnest
 except ImportError:
-    raise Exception("This package needs `cpnest`. To install it follow "
-                    "instructions at https://github.com/johnveitch/cpnest/tree/massively_parallel.")
+    raise Exception(
+        "This package needs `cpnest`. To install it follow instructions at"
+        "https://github.com/johnveitch/cpnest/tree/massively_parallel."
+    )
+
+try:
+    import figaro
+except ImportError:
+    raise Exception(
+        "This package needs `figaro`. To install it follow instructions at"
+        "https://github.com/sterinaldi/figaro."
+    )
 
 
 # see https://stackoverflow.com/a/21621689/1862861 for why this is here
-class build_ext(_build_ext):
-    def finalize_options(self):
-        _build_ext.finalize_options(self)
-        # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
-        self.include_dirs.append(np.get_include())
+# class build_ext(_build_ext):
+#     def finalize_options(self):
+#         _build_ext.finalize_options(self)
+#         # Prevent numpy from thinking it is still in its setup process:
+#         __builtins__.__NUMPY_SETUP__ = False
+#         self.include_dirs.append(np.get_include())
 
 
 with open("requirements.txt") as requires_file:
     requirements = requires_file.read().split("\n")
+#
+# scripts = [
+#     'line-calibration:spectrumpy.bayes_inference.calibration_fit:main'
+# ]
+#
+# modules = [
+#     'spectrumpy/bayes_inference/calibration_fit:main'
+# ]
 
 setup(
     name='spectrumpy',
@@ -34,5 +52,8 @@ setup(
     install_requires=requirements,
     include_dirs=[np.get_include()],
     setup_requires=['numpy~=1.21.5', 'setuptools_scm'],
-    entry_points={},
-    )
+    # py_modules=modules,
+    # entry_points={
+    #     'console_scripts': scripts
+    # },
+)
