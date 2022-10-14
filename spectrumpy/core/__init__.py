95d79d0ceae37a4fc99020758866a01082f25008
d-sanfratello <d.sanfratello@studenti.unipi.it>
2022-10-14 12:34:24 +0200

    Removed old dependencies

--- spectrumpy/core/__init__.py
+++ spectrumpy/core/__init__.py
@@ -1,4 +1,2 @@
 from spectrum import Spectrum
-from spectrum_cropped_image import SpectrumCroppedImage
-from spectrum_rotated_image import SpectrumRotatedImage
 from spectrum_image import SpectrumImage
