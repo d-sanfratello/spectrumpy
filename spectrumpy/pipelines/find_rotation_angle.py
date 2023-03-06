9a1eb2b6745939e24d00df693d9b2031cfb6cb6e
d-sanfratello <d.sanfratello@studenti.unipi.it>
2023-03-06 16:20:44 +0100

    Changed option for pixel bounds, now optional. Corrected bug in postprocessing data loading. Set inferred parameters to be printed on terminal instead than on an external file. Corrected labels in corner plot.

--- spectrumpy/pipelines/find_rotation_angle.py
+++ spectrumpy/pipelines/find_rotation_angle.py
@@ -27,16 +27,28 @@ def main():
                       dest="postprocess", default=False,
                       help="Whether to do a new inference or to open a file "
                            "of samples.")
-    parser.add_option("-b", "--bounds", type="string", dest="bounds",
-                      help="The bounds for the pixels.")
+    parser.add_option("-b", "--bounds", type="string",
+                      dest="bounds", default=None,
+                      help="The bounds for the pixels. If unset, it selects "
+                           "an interval plus or minus 5 sigma around each "
+                           "point.")
     parser.add_option("-B", "--model-bounds", type="string",
-                      dest="mod_bounds",
+                      dest="mod_bounds", default=None,
                       help="The bounds for the model parameters. Write from "
                            "highest to lowest order.")
     (options, args) = parser.parse_args()
 
-    bounds = eval(options.bounds)
-    mod_bounds = eval(options.mod_bounds)
+    if not options.postprocess and options.mod_bounds is None:
+        raise ValueError("When trying a new inference, bounds must be set.")
+
+    if options.bounds is not None:
+        bounds = eval(options.bounds)
+        bounds = np.array(bounds).flatten()
+    else:
+        bounds = options.bounds
+
+    if options.mod_bounds is not None:
+        mod_bounds = eval(options.mod_bounds)
 
     if options.data_file is not None:
         data = np.genfromtxt(options.data_file, names=True)
@@ -51,7 +63,7 @@ def main():
 
     if not options.postprocess:
         fit_model = RotationFit(
-            x, dx, y, dy, bounds, mod_bounds
+            x, dx, y, dy, x_bounds=bounds, mod_bounds=mod_bounds
         )
 
         work = cpnest.CPNest(
@@ -60,7 +72,7 @@ def main():
             nlive=1000,  # 1000
             maxmcmc=5000,  # 5000
             nensemble=1,
-            output=Path(options.out_folder).joinpath(options.model)
+            output=Path(options.out_folder).joinpath('rotation_fit')
         )
         work.run()
         post = work.posterior_samples.ravel()
@@ -84,7 +96,8 @@ def main():
             post = np.asarray(f['rotation_params'])
 
         samples = np.column_stack([
-            post['x_1'], post['x_0']
+            post.T[0],
+            post.T[1]
         ])
 
     a_16, a_50, a_84 = corner.quantile(
@@ -95,29 +108,23 @@ def main():
         samples.T[1], [0.16, 0.5, 0.84])
     q_m, q_p = q_50 - q_16, q_84 - q_50
 
-    fig = plt.figure()
+    print()
+    print(f'angle [deg] =\t{a_50:.3e} +{a_p:.3e} -{a_m:.3e}')
+    print(f'offset [px] =\t{q_50:.3e} +{q_p:.3e} - {q_m:.3e}')
 
-    labels = ['$x_1$ [$deg/px$]', '$x_0$ [$px$]']
-
+    labels = [r'$\alpha$ [deg]', '$x_0$ [px]']
     c = corner.corner(
         samples,
         labels=labels,
         quantiles=[0.16, 0.5, 0.84],
-        fig=fig,
         use_math_text=True,
     )
-
     c.savefig(
         Path(options.out_folder).joinpath(
             f'joint_posterior_rotation.pdf'
         ),
         bbox_inches='tight'
     )
-    with open(
-            Path(options.out_folder).joinpath(f'quantiles_rotation.txt'), 'w+'
-    ) as f:
-        f.write(f'angle =\t{a_50} +{a_p} -{a_m}\n')
-        f.write(f'offset =\t{q_50} +{q_p} - {q_m}')
 
 
 if __name__ == "__main__":
