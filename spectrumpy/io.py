c9eab275fbe0e72d097a0ba39108f4bc857fdc4d
d-sanfratello <d.sanfratello@studenti.unipi.it>
2023-03-09 16:42:03 +0100

    Updated parsing functions to read args from argparse instead of optparse. Added data and bounds parsers.

--- spectrumpy/io.py
+++ spectrumpy/io.py
@@ -25,16 +25,14 @@ class SpectrumPath:
         }
 
 
-def parse_image_path(arg_list,
-                     missing_arg_msg="Missing positional argument.",
+def parse_image_path(args,
+                     data_name,
                      wrong_type_msg="Invalid file extension.",
                      save_output=True,
                      **kwargs):
 
-    if len(arg_list) == 0:
-        raise ValueError(missing_arg_msg)
+    image_path = Path(getattr(args, data_name))
 
-    image_path = Path(arg_list[0])
     if image_path.suffix.lower() in ['.fit', '.fits']:
         image_file = SpectrumPath(image_path, is_lamp=kwargs['is_lamp'])
         image = image_file.images[str(kwargs['image'])]
@@ -58,14 +56,11 @@ def parse_image_path(arg_list,
     return image
 
 
-def parse_spectrum_path(arg_list,
-                        missing_arg_msg="Missing positional argument.",
+def parse_spectrum_path(args,
+                        data_name,
                         wrong_type_msg="Invalid file extension.",
                         **kwargs):
-    if len(arg_list) == 0:
-        raise ValueError(missing_arg_msg)
-
-    spectrum_path = Path(arg_list[0])
+    spectrum_path = Path(getattr(args, data_name))
     if spectrum_path.suffix.lower() in ['.h5', '.hdf5']:
         with h5py.File(spectrum_path, 'r') as f:
             spectrum_data = np.asarray(f['spectrum'])
@@ -75,3 +70,37 @@ def parse_spectrum_path(arg_list,
         raise ValueError(wrong_type_msg)
 
     return spectrum
+
+
+def parse_data_path(arg_list,
+                    data_name,
+                    **kwargs):
+    data_file = Path(getattr(arg_list, data_name))
+    data = np.genfromtxt(data_file, names=True)
+
+    names = data.dtype.names
+
+    x = data[names[0]]
+    dx = data[names[1]]
+    y = data[names[2]]
+    dy = data[names[3]]
+
+    return x, dx, y, dy
+
+
+def parse_bounds(args):
+    if not args.postprocess and args.mod_bounds is None:
+        raise ValueError(
+            "When trying a new inference, bounds must be set."
+        )
+
+    if args.bounds is not None:
+        x_bounds = eval(args.bounds)
+        x_bounds = np.array(x_bounds).flatten()
+    else:
+        x_bounds = args.bounds
+
+    if args.mod_bounds is not None:
+        mod_bounds = eval(args.mod_bounds)
+
+    return x_bounds, mod_bounds
